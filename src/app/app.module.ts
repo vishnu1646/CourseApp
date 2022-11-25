4e5585e0da0d28a57b35d8653af8f1c694a217a6
vishnu1646 <vishnulance98@gmail.com>
2022-11-25 18:12:24 +0530

    viewing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddcourseComponent } from './addcourse/addcourse.component';
+import { ViewcourseComponent } from './viewcourse/viewcourse.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddcourseComponent
+    AddcourseComponent,
+    ViewcourseComponent
   ],
   imports: [
     BrowserModule,
