2014595dae6ac7686a2ca5d8bff82db1dc019254
vishnu1646 <vishnulance98@gmail.com>
2022-11-25 18:33:34 +0530

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddcourseComponent } from './addcourse/addcourse.component';
 import { ViewcourseComponent } from './viewcourse/viewcourse.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddcourseComponent,
-    ViewcourseComponent
+    ViewcourseComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
